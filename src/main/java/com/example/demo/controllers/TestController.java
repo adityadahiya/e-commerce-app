c106a532ed811c3f8a64d7a9e0f92760b6cd56c8
Aditya Dahiya <aditya.dahiya@publicissapient.com>
2023-03-02 19:19:57 +0530

    health check

--- src/main/java/com/example/demo/controllers/TestController.java
+++ src/main/java/com/example/demo/controllers/TestController.java
@@ -1,26 +1,19 @@
 package com.example.demo.controllers;
 
-import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.model.persistence.Item;
-import com.example.demo.model.persistence.repositories.ItemRepository;
 
 @RestController
 @RequestMapping("/health")
-public class ItemController {
+public class TestController {
 
-
-	
 	@GetMapping
 	public ResponseEntity<String> checkHealth() {
-		return "Status: Working";
+		return ResponseEntity.ok("Status: Working");
 	}
 	
 }
